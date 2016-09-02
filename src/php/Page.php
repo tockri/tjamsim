23785d0e8e2ab7284e10198ea19c57a399a74d84
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2016-09-02 11:50:32 +0900

    title修正

--- src/php/Page.php
+++ src/php/Page.php
@@ -46,7 +46,7 @@ END;
 <link rel="shortcut icon" href="img/favicon.ico" type="image/vnd.microsoft.ico" />
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css" />
-<title>{$title} | Cloud simulator</title>
+<title>{$title} | Traffic jam simulator</title>
 {$script}
 END;
         self::echoStyles();
@@ -58,7 +58,7 @@ END;
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/index.php">Cloud simulator</a>
+      <a class="navbar-brand" href="/index.php">Traffic jam simulator</a>
     </div>
   </div>
 </nav>
